aa17dd39251bec91e99aefa4aefd18f157ade71b
ks58596061@gmail.com <ks58596061@gmail.com>
2025-08-30 14:49:49 +0530

    html file

---
+++ login.js
@@ -0,0 +1,17 @@
+function validateForm() {
+    var email = document.querySelector('.input_email[type="text"]').value;
+    var password = document.querySelector('.input_email[type="password"]').value;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+
+    return true;
+}
